fd5f96bd1f430e32ff4b99e7df1d881ead412b98
ErNikhil0 <107608023+ErNikhil0@users.noreply.github.com>
2025-03-07 00:59:07 +0530

    completed

--- Mongo version/backEnd/server.js
+++ Mongo version/backEnd/server.js
@@ -1,13 +1,50 @@
 const express = require('express')
-require('dotenv').config()
+const dotenv = require('dotenv')
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+dotenv.config()
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'Rmbr';
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World2!')
+client.connect();
+
+//get all the password 
+app.get('/', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult)
 })
+// save a password 
+app.post ('/', async (req, res) => {
+  const password = req.body
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.insertOne(password);
+  res.send({success:true, result: findResult})
+})
+
+// delete a password by id
+app.delete ('/', async (req, res) => {
+  const password = req.body
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.deleteOne(password);
+  res.send({success:true, result: findResult})
+}) 
+
 
 app.listen(port, () => {
   console.log(`Example app listening http://localhost:${port}`)
 })
-//get api for storing
